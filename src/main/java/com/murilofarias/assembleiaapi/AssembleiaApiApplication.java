6594b69982d58e3c996038294f87497e3cb58154
murilofarias <murilo145farias@gmail.com>
2022-09-24 11:37:00 -0300

    first commit

---
+++ src/main/java/com/murilofarias/assembleiaapi/AssembleiaApiApplication.java
@@ -0,0 +1,13 @@
+package com.murilofarias.assembleiaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssembleiaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssembleiaApiApplication.class, args);
+	}
+
+}
