e6913d656512557035a73289997dff66b96f9841
murilofarias <murilo145farias@gmail.com>
2022-10-07 19:58:22 -0300

    implemente caso de uso de listar pautas

---
+++ src/main/java/com/murilofarias/assembleiaapi/domain/usecase/ListarPautasUseCase.java
@@ -0,0 +1,24 @@
+package com.murilofarias.assembleiaapi.domain.usecase;
+
+
+import com.murilofarias.assembleiaapi.domain.model.Associado;
+import com.murilofarias.assembleiaapi.domain.model.Pauta;
+import com.murilofarias.assembleiaapi.infra.AssociadoRepository;
+import com.murilofarias.assembleiaapi.infra.PautaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ListarPautasUseCase {
+
+    @Autowired
+    PautaRepository pautaRepository;
+
+    public Page<Pauta> execute(Pageable pageable){
+        Page<Pauta> pautas = pautaRepository.findAll(pageable);
+        return pautas;
+    }
+}
