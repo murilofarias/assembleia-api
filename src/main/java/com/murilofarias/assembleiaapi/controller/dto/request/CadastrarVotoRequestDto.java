14468b26cf1826cf08e83bf2629a6bff9c96fffc
murilofarias <murilo145farias@gmail.com>
2022-10-10 14:02:15 -0300

    Valide campos de CadatrarVotoRequestDto

--- src/main/java/com/murilofarias/assembleiaapi/controller/dto/request/CadastrarVotoRequestDto.java
+++ src/main/java/com/murilofarias/assembleiaapi/controller/dto/request/CadastrarVotoRequestDto.java
@@ -3,9 +3,11 @@ package com.murilofarias.assembleiaapi.controller.dto.request;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
-
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 
 @Getter
@@ -13,10 +15,14 @@ import javax.validation.constraints.NotBlank;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CadastrarVotoRequestDto {
 
-
+    @NotNull(message="pautaId é obrigatória")
     Long pautaId;
 
+    @CPF(message = "cpf não é válido")
+    @NotBlank(message = "cpf não poder ser branco")
     String cpfAssociado;
 
+    @Pattern(regexp = "^[SN]$", message="voto precisa ser 'S' ou 'N'")
+    @NotNull(message="Voto é obrigatório")
     String voto;
 }
