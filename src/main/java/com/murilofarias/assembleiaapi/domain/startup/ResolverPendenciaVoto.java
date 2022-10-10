e393cb27a2eee8bc6008ac762183b70dbe47fdd0
murilofarias <murilo145farias@gmail.com>
2022-10-10 13:08:49 -0300

    Implemente agendamento de resolucão de pendencias na computação de votos

---
+++ src/main/java/com/murilofarias/assembleiaapi/domain/startup/ResolverPendenciaVoto.java
@@ -0,0 +1,58 @@
+package com.murilofarias.assembleiaapi.domain.startup;
+
+import com.murilofarias.assembleiaapi.domain.model.Pauta;
+import com.murilofarias.assembleiaapi.domain.model.SessaoStatus;
+import com.murilofarias.assembleiaapi.domain.model.Voto;
+import com.murilofarias.assembleiaapi.domain.model.VotoStatus;
+import com.murilofarias.assembleiaapi.domain.usecase.FecharSessaoUseCase;
+import com.murilofarias.assembleiaapi.domain.usecase.ValidarVotoUseCase;
+import com.murilofarias.assembleiaapi.infra.PautaRepository;
+import com.murilofarias.assembleiaapi.infra.VotoRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.annotation.Order;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.stereotype.Component;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.List;
+
+
+@Component
+@Order(1)
+class ResolverPendenciaVoto
+        implements ApplicationListener<ApplicationReadyEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(ResolverPendenciaVoto.class);
+
+    @Autowired
+    VotoRepository votoRepository;
+
+    @Autowired
+    ValidarVotoUseCase validarVotoUseCase;
+
+    @Autowired
+    ThreadPoolTaskScheduler taskScheduler;
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        List<Voto> votos = votoRepository.findAllByStatus(VotoStatus.PENDENTE);
+        logger.info("Quantidade de votos pendentes " + votos.size());
+        votos.forEach(voto -> {
+            if((voto.getPauta().getStatus().equals(SessaoStatus.ABERTA) || voto.getPauta().getStatus().equals(SessaoStatus.EM_APURACAO))){
+                taskScheduler.submit(validarVotoUseCase.execute(voto.getId(), voto.getAssociado().getCpf()));
+                logger.info("Voto com id" + voto.getId() + " foi submetido a validação");
+            }
+            else{
+                voto.invalidarVoto();
+                votoRepository.save(voto);
+                logger.info("Voto com id" + voto.getId() + " foi invalidado porque sessão foi realizada");
+            }
+        });
+    }
+}
--- src/main/java/com/murilofarias/assembleiaapi/infra/VotoRepository.java
+++ src/main/java/com/murilofarias/assembleiaapi/infra/VotoRepository.java
@@ -1,6 +1,11 @@
 package com.murilofarias.assembleiaapi.infra;
 
 import com.murilofarias.assembleiaapi.domain.model.Voto;
+import com.murilofarias.assembleiaapi.domain.model.VotoStatus;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface VotoRepository extends PagingAndSortingRepository<Voto, Long> {}
+import java.util.List;
+
+public interface VotoRepository extends PagingAndSortingRepository<Voto, Long> {
+    List<Voto> findAllByStatus(VotoStatus status);
+}
