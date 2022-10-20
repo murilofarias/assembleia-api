dcacdd428c38058e7fac423ee300edfc8e830cb2
murilofarias <murilo145farias@gmail.com>
2022-10-20 14:59:27 -0300

    refactor: Configure parâmetros dos tópicos do kafka, replica e numPartitions, por propriedade em application.properties

--- src/main/java/com/murilofarias/assembleiaapi/configuration/KafkaTopicConfig.java
+++ src/main/java/com/murilofarias/assembleiaapi/configuration/KafkaTopicConfig.java
@@ -1,6 +1,7 @@
 package com.murilofarias.assembleiaapi.configuration;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,13 +9,25 @@ import org.springframework.context.annotation.Configuration;
 public class KafkaTopicConfig {
 
 
+    @Value("${kafka.topic.resultado-sessao.num-partitions: 1}")
+    private Short topicResultadoSessaoNumPartitions;
+
+    @Value("${kafka.topic.resultado-sessao.replicas: 1}")
+    private Short topicResultadoSessaoReplicas;
+
+    @Value("${kafka.topic.votos.num-partitions: 1}")
+    private Short topicVotosNumPartitions;
+
+    @Value("${kafka.topic.votos.replicas: 1}")
+    private Short topicVotosReplicas;
+
     @Bean
     public NewTopic topic2() {
-        return new NewTopic("resultadoSessao", 1, (short) 1);
+        return new NewTopic("resultadoSessao", topicResultadoSessaoNumPartitions, topicResultadoSessaoReplicas);
     }
 
     @Bean
     public NewTopic topic1() {
-        return new NewTopic("votos", 1, (short) 2);
+        return new NewTopic("votos", topicVotosNumPartitions, topicVotosReplicas);
     }
 }
